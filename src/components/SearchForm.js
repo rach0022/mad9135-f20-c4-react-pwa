8bef1081f3b9aea2521660aa5ef21fee2693b0e1
benjaminbradley <hbuhbghi>
2020-11-29 18:23:49 -0500

    Enabled service worker, caching network response

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from "react";
 
-function SearchForm ({ searchTerm, setter }) {
+function SearchForm({ searchTerm, setter }) {
   // when the form gets submitted we want to prevent the default actions
   // maybe display a spinner when it is loading?
-  const submitSearchTerm = ev => {
-    ev.preventDefault()
-  }
+  const submitSearchTerm = (ev) => {
+    ev.preventDefault();
+  };
 
   // return the form and use the searchTerm as the value and set the value with the setter
   return (
-    <form className='container' type='submit' onSubmit={submitSearchTerm}>
-      <div className='col 6'>
+    <form className="container" type="submit" onSubmit={submitSearchTerm}>
+      <div className="col 6">
         <input
-          className='center-align'
-          type='text'
+          className="center-align"
+          type="text"
           value={searchTerm}
-          onChange={ev => setter(ev.target.value)}
-          placeholder='Please search for an alcoholic ingrediant'
+          onChange={(ev) => setter(ev.target.value)}
+          placeholder="Please search for an alcoholic ingredient"
         />
       </div>
     </form>
-  )
+  );
 }
 
-export default SearchForm
+export default SearchForm;
