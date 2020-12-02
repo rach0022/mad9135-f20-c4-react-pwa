3f9d57a12bd3ff4f0782771fed57dbe062fdf96f
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-12-02 12:52:12 -0500

    finialized asignment submission with group

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
-import AppNav from './components/AppNav'
+const AppNav = React.lazy(() => import('./components/AppNav'))
 const CocktailList = React.lazy(() => import('./components/CocktailList'))
 const SearchForm = React.lazy(() => import('./components/SearchForm'))
 const CocktailDetails = React.lazy(() => import('./components/CocktailDetails'))
 const InstallBanner = React.lazy(() => import('./components/InstallBanner'))
+
 function App() {
   const [searchTerm, setSearchTerm] = useState('')
   // const [homeCounter, setHomeCounter] = useState(0)
@@ -13,23 +14,30 @@ function App() {
   return (
     <div className="Random Cocktail App">
       <Router>
-        <AppNav />
         {/* Using React Suspense we can add a loading element */}
         {/* While the componenets are rendering */}
-        <Switch>
-          <Suspense fallback={<div>Loading...</div>}>
+        {/* If it takes too long for an element to load we will show a loading message */}
+        <Suspense fallback={<div>Loading...</div>}>
+          {/* Show the app navigation bar  */}
+          <AppNav />
+
+          <Switch>
 
 
+            {/* Route for the home page */}
             <Route exact path="/">
               <InstallBanner />
               <SearchForm searchTerm={searchTerm} setter={setSearchTerm} />
               <CocktailList searchTerm={searchTerm} />
             </Route>
+
+            {/* Route for the cocktail details page */}
             <Route path="/cocktail/:id">
               <CocktailDetails />
             </Route>
-          </Suspense>
-        </Switch>
+
+          </Switch>
+        </Suspense>
       </Router>
     </div>
   );
